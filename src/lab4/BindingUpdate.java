8678122cc7983425f61e49776778b6306ccf359b
Josefine Bexelius <josbex-4@student.ltu.se>
2020-02-20 14:48:15 +0100

    Forgot to save

--- src/lab4/BindingUpdate.java
+++ src/lab4/BindingUpdate.java
@@ -1,9 +1,17 @@
 package lab4;
 
 import Sim.Event;
+import Sim.NetworkAddr;
 import Sim.SimEnt;
+import lab3.MobileNode;
 
 public class BindingUpdate implements Event {
+	
+	private NetworkAddr foreignAddress;
+	
+	public BindingUpdate(MobileNode node, NetworkAddr foreignAddress) {
+		this.foreignAddress = foreignAddress;
+	}
 
 	public void entering(SimEnt locale) {
 		// TODO Auto-generated method stub
@@ -12,6 +20,10 @@ public class BindingUpdate implements Event {
 	public String getNameofEvent() {
 		// TODO Auto-generated method stub
 		return "Binding Update Event";
+	}
+	
+	public NetworkAddr getForeignAddress() {
+		return this.foreignAddress;
 	}
 
 }
--- src/lab4/MIPNode.java
+++ src/lab4/MIPNode.java
@@ -1,5 +1,22 @@
 package lab4;
 
-public class MIPNode {
+import Sim.NetworkAddr;
+import Sim.Node;
+
+
+public class MIPNode extends Node {
+	
+	private NetworkAddr homeAddress;
+	private HomeAgentRouter homeAgent;
+
+	public MIPNode(int network, int node) {
+		super(network, node);
+		homeAddress = _id;
+	}
+	
+	public void moveTo(HomeAgentRouter newRouter) {
+		_id = new NetworkAddr(newRouter.getNetworkAddr(), _id.nodeId());
+		//send(homeAgent, new BindingUpdate(_id), 0);
+	}
 
 }
