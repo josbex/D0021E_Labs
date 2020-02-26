3ef3583f3e4ac5cf6ace6b2587fdaf14e0d26160
Josefine Bexelius <josbex-4@student.ltu.se>
2020-02-26 17:49:55 +0100

    Forgot to add BindingUpdate check to IdealLink

--- src/Sim/IdealLink.java
+++ src/Sim/IdealLink.java
@@ -3,6 +3,7 @@ package Sim;
 // This class implements a link without any loss, jitter or delay
 
 import lab3.SwitchRouterEvent;
+import lab4.BindingUpdate;
 
 public class IdealLink extends Link {
 	final private int _now = 0;
@@ -14,7 +15,7 @@ public class IdealLink extends Link {
 	// Called when a message enters the link
 
 	public void recv(SimEnt src, Event ev) {
-		if (ev instanceof Message || ev instanceof SwitchRouterEvent) {
+		if (ev instanceof Message || ev instanceof SwitchRouterEvent || ev instanceof BindingUpdate) {
 			this.printMsg("Link recv msg, passes it through");
 			if (src == _connectorA) {
 				send(_connectorB, ev, _now);
